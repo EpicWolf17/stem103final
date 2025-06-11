84acfdfbeb15a9fde51a2636dfa37b66ab7a870e
EpicWolf17 <aimcneely@students.everettcc.edu>
2025-06-11 23:20:53 +0000

    try 1

--- chapters.py
+++ chapters.py
@@ -1,2 +1,16 @@
 def scene_one(player_name):
     print(f"story {player_name} story")
+
+first_option = ["option1, option2, option3, option4"]
+
+def scene_two(choice_one,first_option):
+    if choice_one == first_option[0]:
+        print(f"story")
+    elif choice_one == first_option[1]:
+        print(f"story2")
+    elif choice_one == first_option[2]:
+        print(f"story3")
+    elif choice_one == first_option[3]:
+        print(f"story4")
+    else:
+        input("that isn't a choice please pick a choice.:")
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import random
+#have functions for the each chapter to make code nicer
 from chapters import *
 
 #input from the user and the start of the game
 player_name = input("What is the name of your character?:")
-#have functions for the each chapter to make code nicer
 #opening scene with the player_name
 scene_start = scene_one(player_name)
 #the list for the start
+first_choices = print(first_option)
+choice_one = input("What would you like to do?:")
 #the players choices outcome
+chapter_two = scene_two(choice_one,first_option)
 #the different lists depending on the players choice
 #the next chapter and the story
 #the branching options next options
