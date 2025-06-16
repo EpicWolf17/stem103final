a6dfc879463cbf71fc50f8f977194b7cecf8b9f0
EpicWolf17 <aimcneely@students.everettcc.edu>
2025-06-16 07:07:52 +0000

    thats the third iteration

--- chapters.py
+++ chapters.py
@@ -1,16 +1,108 @@
 def scene_one(player_name):
     print(f"story {player_name} story")
 
-first_option = ["option1, option2, option3, option4"]
+first_option = ["option1, " "option2, " "option3, " "option4"]
+story_one_options = ["option5, " "option6, " "option7, " "option8"]
+story_two_options = ["option9, " "option10, " "option11, " "option12"]
+story_three_options = ["option13, " "option14, " "option15, " "option16"]
+story_four_options = ["option17, " "option18, " "option19, " "option20"]
 
-def scene_two(choice_one,first_option):
-    if choice_one == first_option[0]:
-        print(f"story")
-    elif choice_one == first_option[1]:
+def scene_two(choice_one):
+    if choice_one == "option1":
+        print(f"story1")
+        print(story_one_options)
+        second_option_one = input("What should I do next?:")
+        scene_three_one(second_option_one)
+    elif choice_one == "option2":
         print(f"story2")
-    elif choice_one == first_option[2]:
+        print(story_two_options)
+        second_option_two = input("what should I do next?:")
+        scene_three_two(second_option_two)
+    elif choice_one == "option3":
         print(f"story3")
-    elif choice_one == first_option[3]:
-        print(f"story4")
+        print(story_three_options)
+        second_option_three = input("what should I do next?:")
+        scene_three_three(second_option_three)
     else:
-        input("that isn't a choice please pick a choice.:") 
+        choice_one == "option4"
+        print(f"story4")
+        print(story_four_options)
+        second_option_four = input("what should I do next?:")
+        scene_three_four(second_option_four)
+
+
+
+def scene_three_one(second_option_one):
+    if second_option_one == "option5":
+        print("story5")
+    elif second_option_one == "option6":
+        print("story6")
+    elif second_option_one == "option7":
+        print("story7")
+    else:
+        second_option_one == "option8"
+        print("story8")
+
+def scene_three_two(second_option_two):
+    if second_option_two == "option9":
+        print("story9")
+    elif second_option_two == "option10":
+        print("story10")
+    elif second_option_two == "option11":
+        print("story11")
+    else:
+        second_option_two == "option12"
+        print("story12")
+
+def scene_three_three(second_option_three):
+    if second_option_three == "option13":
+        print("story13")
+    elif second_option_three == "option14":
+        print("story14")
+    elif second_option_three == "option15":
+        print("story15")
+    else:
+        second_option_three == "option16"
+        print("story16")
+
+def scene_three_four(second_option_four):
+    if second_option_four == "option17":
+        print("story17")
+    elif second_option_four == "option18":
+        print("story18")
+    elif second_option_four == "option19":
+        print("story19")
+    else:
+        second_option_four == "option20"
+        print("story20")
+
+
+
+
+
+
+
+
+
+
+def restart_scene(restarting_game):
+    if restarting_game == "yes":
+        print("OKAY WONDERFUL lets go to the start then.")
+    elif restarting_game == "no":
+        print("Okay it was fun adventuring with you! BYE")
+    else:
+        print("Sorry thats not a correct answer")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import random
 #have functions for the each chapter to make code nicer
 from chapters import *
 
+
 #input from the user and the start of the game
 player_name = input("What is the name of your character?:")
 #opening scene with the player_name
@@ -10,12 +11,12 @@ scene_start = scene_one(player_name)
 first_choices = print(first_option)
 choice_one = input("What would you like to do?:")
 #the players choices outcome
-chapter_two = scene_two(choice_one,first_option)
+chapter_two = scene_two(choice_one)
 #the different lists depending on the players choice
-#the next chapter and the story
-#the branching options next options
 #the ending of the game depending on the choices they made
 #ask if they want to play again
+restarting_game = input("Would you like to play again? Please put Yes or No:")
+restart_scene(restarting_game)
 #send them back to the start if they want to play again
 
 
